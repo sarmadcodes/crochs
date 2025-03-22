9465d00354b9ebf5cf48958f497bc54ee93b43ee
Sarmad Abbasi <sarmadsuper3@gmail.com>
2025-03-22 07:23:34 +0500

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,22 +1,208 @@
 import { Product } from '../types';
 
 export const products: Product[] = [
-  { id: 1, name: "balalaaa", price: 45, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Blankets" },
-  { id: 2, name: "Amigurumi Bunny", price: 25, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Toys" },
-  { id: 3, name: "Flower Coasters (Set of 4)", price: 18, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Home" },
-  { id: 4, name: "Strawberry Beanie", price: 22, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Accessories" },
-  { id: 5, name: "Heart Garland", price: 30, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Decor" },
-  { id: 6, name: "Crochet Plant Hanger", price: 15, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Home" },
-  { id: 7, name: "Granny Square Throw", price: 65, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Blankets" },
-  { id: 8, name: "Amigurumi Elephant", price: 28, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Toys" },
-  { id: 9, name: "Rainbow Wall Hanging", price: 35, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Decor" },
-  { id: 10, name: "Slouchy Winter Hat", price: 24, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Accessories" },
-  { id: 11, name: "Cotton Market Bag", price: 32, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Accessories" },
-  { id: 12, name: "Baby Booties", price: 18, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Baby" },
-  { id: 13, name: "Pumpkin Decorations (Set of 3)", price: 27, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Seasonal" },
-  { id: 14, name: "Chunky Throw Pillow", price: 40, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Home" },
-  { id: 15, name: "Fingerless Gloves", price: 20, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Accessories" },
-  { id: 16, name: "Cactus Collection (Set of 2)", price: 22, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Decor" },
-  { id: 17, name: "Pumpkin Decorations (Set of 2)", price: 27, image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", category: "Seasonal" },
-  
+  { 
+    id: 1, 
+    name: "balalaaa", 
+    price: 45, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Blankets",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 2, 
+    name: "Amigurumi Bunny", 
+    price: 25, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Toys",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 3, 
+    name: "Flower Coasters (Set of 4)", 
+    price: 18, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Home",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 4, 
+    name: "Strawberry Beanie", 
+    price: 22, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Accessories",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 5, 
+    name: "Heart Garland", 
+    price: 30, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Decor",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 6, 
+    name: "Crochet Plant Hanger", 
+    price: 15, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Home",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 7, 
+    name: "Granny Square Throw", 
+    price: 65, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Blankets",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 8, 
+    name: "Amigurumi Elephant", 
+    price: 28, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Toys",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 9, 
+    name: "Rainbow Wall Hanging", 
+    price: 35, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Decor",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 10, 
+    name: "Slouchy Winter Hat", 
+    price: 24, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Accessories",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 11, 
+    name: "Cotton Market Bag", 
+    price: 32, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Accessories",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 12, 
+    name: "Baby Booties", 
+    price: 18, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Baby",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 13, 
+    name: "Pumpkin Decorations (Set of 3)", 
+    price: 27, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Seasonal",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 14, 
+    name: "Chunky Throw Pillow", 
+    price: 40, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Home",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 15, 
+    name: "Fingerless Gloves", 
+    price: 20, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Accessories",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 16, 
+    name: "Cactus Collection (Set of 2)", 
+    price: 22, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Decor",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
+  { 
+    id: 17, 
+    name: "Pumpkin Decorations (Set of 2)", 
+    price: 27, 
+    image: "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg", 
+    category: "Seasonal",
+    images: [
+      "https://i.pinimg.com/736x/8f/85/87/8f8587df52d364a4109a675399eedcc8.jpg",
+      "https://i.pinimg.com/736x/2c/70/c9/2c70c9554554ae887c9391c0b0ea3b96.jpg",
+      "https://i.pinimg.com/736x/78/82/f8/7882f8d3a2873d410e0a631137952d4b.jpg"
+    ]
+  },
 ];
