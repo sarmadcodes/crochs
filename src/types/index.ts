d5efdedf0a2f39be54c987ed0f0a5c9582d71631
Sarmad Abbasi <sarmadsuper3@gmail.com>
2025-03-22 07:23:02 +0500

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ export interface Product {
   price: number;
   image: string;
   category: string;
+  images?: string[]; // Added this field for multiple images
 }
 
 export interface CartItem extends Product {
