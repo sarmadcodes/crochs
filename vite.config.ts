407ddc96a2cdf1cd231a50e7eb2c4cb7080e1ca2
Sarmad Abbasi <sarmadsuper3@gmail.com>
2025-04-17 13:39:52 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +10,12 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
+    include: ['firebase/app', 'firebase/firestore', 'firebase/storage'],
     exclude: ['lucide-react'],
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
-  
