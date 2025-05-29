57cc1f97605cc335df85c7c18f9ed92ce3dea122
Sarmad Abbasi <sarmadsuper3@gmail.com>
2025-05-29 08:05:07 +0500

    Update Firebase.tsx

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-import {getFirestore} from "@firebase/firestore";
+import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDK4T_ATg53shwfoWbpZigwBlzLVL2TbYw",
   authDomain: "crochet-store-e7da1.firebaseapp.com",
   projectId: "crochet-store-e7da1",
-  storageBucket: "crochet-store-e7da1.firebasestorage.app",
+  storageBucket: "crochet-store-e7da1.appspot.com", // Fixed format
   messagingSenderId: "546950482110",
   appId: "1:546950482110:web:36a7fd95157350246e36ef",
   measurementId: "G-TYRDPXKXZC"
@@ -17,3 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
